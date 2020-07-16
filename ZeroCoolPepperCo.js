9ef82ee04186fab5533c5b0ffe34bb319022ab2f
Ely Sanneman <65604667+thisguyEly@users.noreply.github.com>
2020-07-15 23:52:24 -0400

    Add files via upload

--- ZeroCoolPepperCo.js
+++ ZeroCoolPepperCo.js
@@ -3,5 +3,35 @@ document.addEventListener('DOMContentLoaded',() => {
 })
 
 /**
- * 
+ * Image Carousel
  */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/Previous Button Controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail Image Controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[1].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].lastChild;
+}
